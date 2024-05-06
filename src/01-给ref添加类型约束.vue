2bea86a62c39da27004ad52ea52560e2d77fabc6
bugsmo <kalandramo@gmail.com>
2024-05-06 22:46:34 +0800

    给vue响应式ref、reactive添加类型约束

---
+++ src/01-给ref添加类型约束.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+import { ref } from "vue";
+// 1. 给简单类型添加类型约束
+// 方式1：使用类型推导
+const count = ref(100)
+console.log(count.value);
+
+// 方式2：手动指定类型，使用泛型
+const msg=ref<string>('I am string')
+console.log(msg.value);
+
+// 2. 给复杂类型添加类型约束
+const year = ref<number|string>(2024)
+year.value='2025'
+console.log(year.value);
+
+// 3. 思考题
+type Goods = {
+  id: string,
+  name: string,
+  price: number
+}
+
+const list = ref<Goods[]>([])
+list.value=[
+    {
+    id: '1001',
+    name: '男士卫衣',
+    price: 100,
+  },
+  {
+    id: '1002',
+    name: '男士裤子',
+    price: 200,
+  }
+]
+
+
+
+</script>
+
+<template>
+  <ul>
+    <li  v-for="item in list" :key="item.id">
+      {{ item.name }}---{{item.price}}
+    </li>
+  </ul>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/App.vue
@@ -0,0 +1,58 @@
+<script setup lang="ts">
+import { reactive } from "vue";
+// 1. 给 reactive 添加类型约束
+// 方式1：使用类型推导
+const form = reactive({
+  username: 'test',
+  password:'123123',
+})
+
+console.log(form);
+
+
+// 方式2：手动指定类型
+type LoginForm = {
+  username: string,
+  password: string,
+  isGreed?: bool,
+}
+
+const login: LoginForm=reactive({
+  username:'test1',
+  password:"123321",
+})
+
+login.isGreed=true
+
+console.log(login);
+
+// 思考题
+type address = {
+    username: string,
+    address:string,
+    phone:string,
+    gender?:'男'|'女'
+}
+
+const myAddress: address=reactive(
+  {
+    username: 'you',
+    address: 'cn',
+    phone: '1234566778',
+  }
+)
+
+myAddress.gender='男'
+
+console.log(myAddress);
+
+
+
+</script>
+
+<template>
+</template>
+
+<style scoped>
+
+</style>
