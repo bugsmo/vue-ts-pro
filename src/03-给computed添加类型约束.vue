0593ed243d2df1bbbc82535c71c7a92cdf5f5b9b
bugsmo <kalandramo@gmail.com>
2024-05-07 22:08:27 +0800

    模板引用添加类型约束

---
+++ src/03-给computed添加类型约束.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import { ref,computed } from "vue";
+// 目标：computed 的类型约束
+
+// 方式：computed会自动推导，无需配置，也不应该配置
+const count=ref(10)
+
+const doubleCount=computed(()=>{
+  return count.value*2
+})
+
+console.log(doubleCount.value);
+
+// 练习题
+type Goods = {
+  id:string,
+  name:string,
+  price:number,
+}
+
+const goodsList=ref<Goods[]>([])
+goodsList.value=[
+  {id:'1001',name:'男鞋',price:550},
+  {id:'1002',name:'女鞋',price:850},
+  {id:'1003',name:'衬衫',price:450},
+]
+
+const newList=computed(()=>{
+  return goodsList.value.filter(item=>{
+    return item.price>500
+    }
+  )
+})
+</script>
+
+<template>
+  <ul>
+    <li v-for="item in newList" :key="item.id">
+    {{ item.name }}---{{ item.price }}
+    </li>
+  </ul>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/04-给事件处理函数添加类型约束.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+
+// 目标：为事件处理函数添加类型约束
+
+// 给事件形参标注为 Event 类型，获得 input 标签的智能提示
+const inputChange=(e:Event)=>{
+  // 使用 as 断言获得更进一步的提示
+  console.log((e.target as HTMLInputElement).value);
+}
+
+// 练习题
+const buttonClick=(e:Event)=>{
+  console.log((e.target as HTMLButtonElement));
+}
+
+</script>
+
+<template>
+<input type="text" @change="inputChange">
+<button @click="buttonClick">按钮</button>
+</template>
+
+<style scoped>
+
+</style>
